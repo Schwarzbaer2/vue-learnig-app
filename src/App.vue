d892e6501b2750e5d31b3f6dfb65ffb491ce0396
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-03-03 10:36:43 +0100

    Adding and styling tabs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
   <TheHeader title="Remember me" />
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader';
+import TheResources from './components/learning-resources/TheResources';
 
 export default {
-  components: { TheHeader },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://vuejs.org'
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.de'
-        }
-      ]
-    };
-  }
+  components: { TheHeader, TheResources }
 };
 </script>
 
